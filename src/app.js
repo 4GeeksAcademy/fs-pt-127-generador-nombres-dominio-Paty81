1c470f30ef8db5cf60997dd0be3113fb169264c4
Paty81 <molinamartosp@gmail.com>
2025-11-24 10:38:57 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const pronouns = ['the','our'];
+  const adjs = ['great', 'big'];
+  const nouns = ['jogger', 'racoon'];
+
+  const  domains = [];
+
+pronouns.forEach((pronoun)=> {
+  adjs.forEach((adj) => {
+    nouns.forEach((noun)=> {
+      domains.push(`${pronoun}${adj}${noun}.com`)
+    })
+    })
+  })
+  console.log(domains)
+}
+  
+
+
